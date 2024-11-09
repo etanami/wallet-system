f67c1638131dbd67102fd2548c22c60911927946
Etanami Olatunji <etanamiolatunji@gmail.com>
2024-11-09 17:57:46 +0100

    fix: harshed user password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/db/user.entity';
 import { CreateUserDto } from 'src/dtos/createUser.dto';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -13,7 +14,13 @@ export class UsersService {
 
   // Creates new user from DTO and saves to database
   public async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const user = this.userRepository.create(createUserDto);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    const user = this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
     return this.userRepository.save(user);
   }
 
